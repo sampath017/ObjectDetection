cfd5e8fd55510bc4db82e1956d661f25f8bfe357
sampath <118274231+sampath017@users.noreply.github.com>
2024-12-05 18:02:20 +0000

    ready to do data augumentations

--- src/callbacks.py
+++ src/callbacks.py
@@ -8,3 +8,30 @@ class OverfitCallback:
             self.limit_val_batches = limit_val_batches
 
         self.max_epochs = max_epochs
+
+
+class EarlyStoppingCallback:
+    def __init__(self, wait_epochs=3, accuracy_diff=2.0, min_val_accuracy=90.0):
+        if wait_epochs < 0:
+            raise ValueError("wait_epochs should be > 0!")
+
+        self.wait_epochs = wait_epochs
+        self.accuracy_diff = accuracy_diff
+        self.waited_epochs = 0
+        self.min_val_accuracy = min_val_accuracy
+        self.check_early_stopping = False
+
+    def check(self, epoch_train_accuracy, epoch_val_accuracy):
+        stop_training = False
+        if not self.check_early_stopping and (epoch_val_accuracy >= self.min_val_accuracy):
+            print("Early stopping check started!")
+            self.check_early_stopping = True
+
+        if self.check_early_stopping:
+            diff = epoch_train_accuracy - epoch_val_accuracy
+            if diff > self.accuracy_diff:
+                self.waited_epochs += 1
+                if self.waited_epochs >= self.wait_epochs:
+                    stop_training = True
+
+        return stop_training, self.accuracy_diff
