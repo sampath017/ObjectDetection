10652b35ad68c0bce99ce492fc53077c75570e8e
sampath <118274231+sampath017@users.noreply.github.com>
2024-12-13 13:25:21 +0000

    working on ResNet 20 vs ResNet 50

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,18 @@ import wandb
 
 
 class WandbLogger:
-    def __init__(self, config, project_name, logs_path, notes=None):
+    def __init__(self, config, project_name, logs_path, notes=None, offline=False):
         self.project_name = project_name
         self.config = config
         self.logs_path = logs_path
         self.notes = notes
+        self.mode = "offline" if offline else "online"
 
     def init(self):
         wandb.init(
             project=self.project_name,
             config=self.config,
             dir=self.logs_path,
-            notes=self.notes
+            notes=self.notes,
+            mode=self.mode
         )
--- src/module.py
+++ src/module.py
@@ -4,7 +4,57 @@ from torch import nn
 from utils import accuracy
 
 
-class VGGBlock(nn.Module):
+class ResidualNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.feature_extractor = nn.Sequential(
+            ConvBlock(3, 8, 8, 16, max_pool=True),
+            ConvBlock(16, 32, 32, 64, max_pool=True),
+            ConvBlock(64, 128, 128, 512, max_pool=True),
+            ResidualBlock(512, max_pool=True),
+            ResidualBlock(512),
+            ResidualBlock(512),
+            ResidualBlock(512),
+            ResidualBlock(512),
+            ResidualBlock(512),
+            # ResidualBlock(512, 512, 512, 512),
+
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            # ResidualBlock(512, 512, 512, 512),
+            ResidualBlock(512, last_block=True)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(2048, 128),
+            nn.ReLU(),
+
+            nn.Linear(128, 10),
+        )
+
+    def forward(self, x):
+        features = self.feature_extractor(x).flatten(start_dim=1)
+        logits = self.classifier(features)
+
+        return logits
+
+
+class ConvBlock(nn.Module):
     def __init__(
         self,
         block1_in_channels=3,
@@ -51,56 +101,52 @@ class VGGBlock(nn.Module):
         return self.block(x)
 
 
-class VGGNet(nn.Module):
-    def __init__(self):
+class ResidualBlock(nn.Module):
+    def __init__(
+        self,
+        channels=3,
+        max_pool=False,
+        last_block=False,
+    ):
         super().__init__()
-        self.feature_extractor = nn.Sequential(
-            VGGBlock(3, 8, 8, 16, max_pool=True),
-            VGGBlock(16, 32, 32, 64, max_pool=True),
-            VGGBlock(64, 128, 128, 512, max_pool=True),
-            VGGBlock(512, 512, 512, 512, max_pool=True),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-
-
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-
-
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512),
-            VGGBlock(512, 512, 512, 512, last_block=True)
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(2048, 128),
+        layers = [
+            # Block 1
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=(3, 3),
+                stride=1,
+                padding=1,
+                bias=False
+            ),
+            nn.BatchNorm2d(num_features=channels),
             nn.ReLU(),
 
-            nn.Linear(128, 10),
-        )
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=(3, 3),
+                stride=1,
+                padding=1,
+                bias=True if last_block else False
+            )
+        ]
+
+        if not last_block:
+            layers.extend([nn.BatchNorm2d(num_features=channels),
+                           nn.ReLU(),])
+        self.block = nn.Sequential(*layers)
+
+        self.max_pool = nn.MaxPool2d(kernel_size=(
+            2, 2), stride=2) if max_pool else None
 
     def forward(self, x):
-        features = self.feature_extractor(x).flatten(start_dim=1)
-        logits = self.classifier(features)
+        x = self.block(x) + x
 
-        return logits
+        if self.max_pool:
+            x = self.max_pool(x)
+
+        return x
 
 
 class QuickModule:
@@ -126,10 +172,10 @@ class QuickModule:
         self.optimizer_func.load_state_dict(checkpoint["optimizer"])
 
 
-class VGGNetModule(QuickModule):
+class ResidualNetModule(QuickModule):
     def __init__(self):
         super().__init__()
-        self.model = VGGNet()
+        self.model = ResidualNet()
 
     def training_step(self, batch):
         loss, acc = self.forward(batch)
